d818950b9b750cb7c55f69eccacdd83f78e517b6
mollerhoj <mollerhoj3@gmail.com>
2017-09-13 14:50:24 +0200

    bulk

--- app/linguist.py
+++ app/linguist.py
@@ -2,22 +2,39 @@ import os.path
 import csv
 
 from .conllu_to_json_converter import convert
-from .train import train
+from .train import train_run
 from jinja2 import Template
 from pathlib import Path
 
+from distutils.dir_util import copy_tree
 
 def run(_dir, _code, _name):
-    output(_dir, 'stop_words.py', read_csv(_dir, _code, 'stop_words.csv'))
-    output(_dir, 'lemmatizer.py', read_csv(_dir, _code, 'lemmas.csv'))
-    output(_dir, 'norm_exceptions.py', read_csv(
+    output(_dir, _code, 'stop_words.py', read_csv(_dir, _code, 'stop_words.csv'))
+    output(_dir, _code, 'lemmatizer.py', read_csv(_dir, _code, 'lemmas.csv'))
+    output(_dir, _code, 'norm_exceptions.py', read_csv(
         _dir, _code, 'same_spelling.csv'))
-    output(_dir, 'tokenizer_exceptions.py', read_csv(
+    output(_dir, _code, 'tokenizer_exceptions.py', read_csv(
         _dir, _code, 'contracted_words.csv'))
-    output(_dir, 'morph_rules.py', read_csv(
+    output(_dir, _code, 'morph_rules.py', read_csv(
         _dir, _code, 'personal_pronouns.csv'))
-    output(_dir, '__init__.py', [_name, _code])
+    output(_dir, _code, '__init__.py', [_name, _code])
     convert_ud(_dir, _code)
+    train_run(_dir,_code)
+    add_language(_dir, _code)
+
+def add_language(_dir, _code):
+    path = os.path.join(_dir, '..', 'spaCy', 'spacy', 'data', _code)
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    string = render_template(_dir, 'language__init__.py', {})
+
+    with open(os.path.join(path, '__init__.py'), "w") as f:
+        f.write(string)
+
+    # copy subdirectory
+    name = _code + "_bothub"
+    copy_tree(os.path.join(_dir, '..', 'models', 'model4'), os.path.join(path, name))
 
 
 def read_csv(_dir, _code, filename):
@@ -33,10 +50,10 @@ def read_csv(_dir, _code, filename):
     return result
 
 
-def output(_dir, filename, data):
+def output(_dir, _code, filename, data):
     if data is None:
         return
-    write_template(_dir, filename, render_template(_dir, filename, data))
+    write_template(_dir, _code, filename, render_template(_dir, filename, data))
 
 
 def render_template(_dir, filename, data):
@@ -45,8 +62,12 @@ def render_template(_dir, filename, data):
         return tmpl.render(data=data)
 
 
-def write_template(_dir, filename, string):
-    with open(os.path.join(_dir, 'output', filename), "w") as f:
+def write_template(_dir, _code, filename, string):
+    output_path = os.path.join(_dir, '..', 'spaCy', 'spacy', 'lang', _code)
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    with open(os.path.join(output_path, filename), "w") as f:
         f.write(string)
 
 
--- app/train.py
+++ app/train.py
@@ -3,14 +3,16 @@ import os.path
 
 current_dir = os.path.dirname(__file__)
 
-no_entities = True
-cmd = 'train'
-lang = 'da'
-output_dir = current_dir
-train_data = current_dir + "/../output/ud_train.jsonu"
-dev_data = current_dir + "/../output/ud_dev.jsonu"
-n_iter = 5
-n_sents = 10
+def train_run(_dir, _code):
+    models_path = os.path.join(_dir, '..', 'models')
+    if not os.path.exists(models_path):
+        os.makedirs(models_path)
 
-# def run():
-#     train(cmd, lang, output_dir, train_data, dev_data, n_iter, n_sents, no_entities=True)
+    train_data = current_dir + "/../output/ud_train.jsonu"
+    dev_data = current_dir + "/../output/ud_dev.jsonu"
+    no_entities = True
+
+    n_iter = 5
+    n_sents = 10
+
+    train('train', _code, models_path, train_data, dev_data, n_iter, n_sents, no_entities=True)
