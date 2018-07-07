9e5529935556a559aa9e667eae9a0404283c80ac
John Cordeiro <john.dalton@ilhasoft.com.br>
2018-07-07 10:13:43 -0300

    Add final steps of creating the model

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,16 @@
-python -m app $LANG_ISO $LANG_NAME $TRANING_TYPE
+export MODEL_NAME=${LANG_ISO}_vectors_web_lg-1.0.0
 
+python -m app $LANG_ISO $LANG_NAME $TRANING_TYPE
 python -m spacy package ../models/$LANG_ISO/ output/
 
-cd output/${LANG_ISO}_vectors_web_lg-1.0.0
+cd output/$MODEL_NAME
 python setup.py sdist
+
+aws s3 cp dist/${MODEL_NAME}.tar.gz s3://bothub-models/spacy/${MODEL_NAME}.tar.gz
+
+cd $SPACY_HOME
+
+git add .
+git commit -m "Add ${LANG_NAME} language files"
+git push origin feature/processing
+
