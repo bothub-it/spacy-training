6d9e62dd915b3c8dec93fe061f8444e340c68539
John Cordeiro <john.dalton@ilhasoft.com.br>
2018-07-07 18:29:58 -0300

    Refactor argument order putting overwrite to the end

--- app/__main__.py
+++ app/__main__.py
@@ -6,9 +6,9 @@ parser = argparse.ArgumentParser(description='Add a new language to spaCy.')
 
 parser.add_argument('code', type=str, help='The language code of the target language')
 parser.add_argument('name', type=str, help='The name of the target language')
-parser.add_argument('overwrite', type=bool, default=True, help='Should overwrite language files')
 parser.add_argument('type', type=str, choices=[TYPE_UNIVERSAL_DEPS, TYPE_FAST_TEXT],
                     help='The type of the model')
+parser.add_argument('overwrite', type=bool, default=True, help='Should overwrite language files')
 
 args = parser.parse_args()
 
