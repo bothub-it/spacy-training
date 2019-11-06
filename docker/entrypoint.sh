b84bc68836cb52c3e8a1630c75330875ef9c797a
Dielson Sales <dielson@ilhasoft.com.br>
2019-11-06 10:50:01 -0300

    Update Docker files
    * Remove commits to the old forked SpaCy repository;
    * Fix mistype in TRAINING_TYPE;
    * Make bucket name configurable through .env file;
    * Update to SpaCy 2.1.9;

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,27 +1,29 @@
-export MODEL_NAME=${LANG_ISO}_vectors_web_${LANG_SIZE}-1.0.0
+#! /bin/sh
+export MODEL_NAME=${LANG_ISO}_vectors_web_${LANG_SIZE}-1.1.0
 
-python -m app $LANG_ISO $LANG_NAME $TRANING_TYPE $OVERWRITE_LANG $LANG_SIZE $PRUNE_VECTORS $DOWNLOAD_SOURCE
+python -m app ${LANG_ISO} ${LANG_NAME} ${TRAINING_TYPE} ${OVERWRITE_LANG} ${LANG_SIZE} ${PRUNE_VECTORS} ${DOWNLOAD_SOURCE}
 
 mkdir output/
-python -m spacy package ../models/$LANG_ISO/ output/
+python -m spacy package ../models/${LANG_ISO}/ output/
 
-cd output/$MODEL_NAME
+cd output/${MODEL_NAME}
 python setup.py sdist
 
-aws s3 cp dist/${MODEL_NAME}.tar.gz s3://bothub-models/spacy/${MODEL_NAME}.tar.gz
+aws s3 cp dist/${MODEL_NAME}.tar.gz s3://${AWS_BUCKET_NAME}/${MODEL_NAME}.tar.gz
 
-if [ -z $OVERWRITE_LANG ] || [ $OVERWRITE_LANG != 'False' ]
-then
-    cd $SPACY_HOME
-
-    git add .
-    git -c user.name="$GITHUB_USERNAME" -c user.email="$GITHUB_EMAIL" commit -m "Add ${LANG_NAME} language files"
-
-    git remote set-url origin git+ssh://git@github.com/ilhasoft/spaCy.git
-
-    ssh-keyscan github.com >> /root/.ssh/known_hosts
-
-    git -c user.name="$GITHUB_USERNAME" -c user.email="$GITHUB_EMAIL" pull origin feature/processing --no-edit
-    git push origin feature/processing
-fi
+# Commenting this out because we're not forking the original Rasa repo anymore
+# if [ -z $OVERWRITE_LANG ] || [ $OVERWRITE_LANG != 'False' ]
+# then
+#     cd $SPACY_HOME
+#
+#     git add .
+#     git -c user.name="$GITHUB_USERNAME" -c user.email="$GITHUB_EMAIL" commit -m "Add ${LANG_NAME} language files"
+#
+#     git remote set-url origin git+ssh://git@github.com/ilhasoft/spaCy.git
+#
+#     ssh-keyscan github.com >> /root/.ssh/known_hosts
+#
+#     git -c user.name="$GITHUB_USERNAME" -c user.email="$GITHUB_EMAIL" pull origin feature/processing --no-edit
+#     git push origin feature/processing
+# fi
 
